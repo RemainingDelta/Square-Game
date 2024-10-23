c3e773760edf46776bcfe51adc7977e7df48b6f1
Shiven Ajwaliya <146774012+RemainingDelta@users.noreply.github.com>
2024-10-23 01:06:48 -0400

    Version 2

--- main.py
+++ main.py
@@ -10,18 +10,22 @@ def squares_picture(number_string):
    # Loop through each number and print the corresponding number of 'X's
    for number in numbers:
        count = int(number)  # Convert the string to an integer
-       #print('X' * count)   # Print 'X' repeated 'count' times
+       print('X' * count)   # Print 'X' repeated 'count' times
        squares_array.append(count)
        binary_array.append(int(bin(count)[2:]))
    print (squares_array)
    print (binary_array)
 
-
-
-
 squares_picture(squares)
 
 
+def xor(array):
+    xor_value = 0
+    for i in range(len(array)):
+        xor_value = xor_value ^ array[i]
+    return xor_value
+
+print (xor(binary_array))
 
 
 
